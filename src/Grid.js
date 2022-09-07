3ef0bfd58fbc17a53bad464923efb29a56a3ac15
Sylar <user@Sylar-MacBook-Pro.local>
2022-09-07 16:19:14 +0100

    updated react

--- src/Grid.js
+++ src/Grid.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import './css/fonts.css';   
-import './css/Grid.css';    
+import React from "react";
+import "./css/fonts.css";
+import "./css/Grid.css";
 
-import HorizontalBarGraph from '@chartiful/react-horizontal-bar-graph'
+import HorizontalBarGraph from "@chartiful/react-horizontal-bar-graph";
 
-export default function Grid () {
-  
+export default function Grid() {
   return (
-    <><><div className="grid-container">
-      <div className="item"></div>
-      <div class="mid-col">Current Season</div>
-      <div className="item"></div>
-      <div className="item"></div> 
-      <div class="mid-col">All Time</div>
-      <div className="item"></div>
-    </div></></> 
-  )
+    <>
+      <>
+        <div className="grid-container">
+          <div className="item"></div>
+          <div className="mid-col">Current Season</div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="mid-col">All Time</div>
+          <div className="item"></div>
+        </div>
+      </>
+    </>
+  );
 }
--- src/footer.js
+++ src/footer.js
@@ -1,12 +1,18 @@
 import React from "react";
-import './css/footer.css';  
-import './css/fonts.css'; 
-import './css/App.css';  
+import "./css/footer.css";
+import "./css/fonts.css";
+import "./css/App.css";
 
 const footer = () => (
   <div className="bottom-container">
-    <h3>Made with <i class="fa-brands fa-react-small"></i> ReactJS by Tedley Meralus</h3> 
-    <a href="https://twitter.com/techgameteddy"> <i class="fa-brands fa-twitter"></i></a> 
+    <h3>
+      Made with <i className="fa-brands fa-react-small"></i> ReactJS by Tedley
+      Meralus
+    </h3>
+    <a href="https://twitter.com/techgameteddy">
+      {" "}
+      <i className="fa-brands fa-twitter"></i>
+    </a>
   </div>
 );
 
